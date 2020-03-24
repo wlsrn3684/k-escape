87158718742f155f0cda0cadeb319cf6882382de
wlsrn3684 <50126441+wlsrn3684@users.noreply.github.com>
2020-03-24 17:25:48 +0900

    make graph

--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,20 @@
 $(document).ready( () => {
-    
-    var line = document.getElementById('line')
+
+    var line = document.getElementById('myline')
 
     var line_data = {
-
+        labels: ['1학년1학기', '1학년2학기', '2학년1학기', '2학년2학기', '3학년1학기', '3학년2학기', '4학년1학기', '4학년2학기'],
+        datasets: [{
+            label: '전공학점',
+            backgroundColor: 'rgba(255, 99, 132, 0.1)',
+            data: [ 0, 0, 3.6, 3.7, 3.5, 3.5, 3.5, 3.5]
+        }
+        ,
+        {
+            label: '전체학점',
+            backgroundColor: 'rgba(22, 0, 132, 0.1)',
+            data: [4.0, 3, 3, 4, 3, 4, 4]
+        }]
     }
 
     var myline = new Chart( line, {
@@ -11,19 +22,27 @@ $(document).ready( () => {
         data: line_data,
         options: {}
     })
-    
-    var doughnut = document.getElementById('doughnut').getContext('2d')
+
+
+    var doughnut = document.getElementById('mydoughnut').getContext('2d')
 
     var doughnut_data = {
-        dataset: [{
-            data: [10, 20, 30]
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: [
+                "rgb(255, 99, 132)",
+                "rgb(54, 162, 235)",
+            ]
         }],
+    
         labels: [
-            'RED',
-            'YELLOW',
-            'BLUE'
-        ]
+            '전공',
+            '교양',
+            '미이수'
+        ],
+
     }
+
 
     var mydoughnut = new Chart( doughnut, {
         type: 'doughnut',
