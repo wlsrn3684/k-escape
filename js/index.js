dfb8e7bc09322496665141e2d6798919fcef85f4
wlsrn3684 <50126441+wlsrn3684@users.noreply.github.com>
2020-03-27 21:18:40 +0900

    add sidebar toggle css

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function(){   
 
+    setTimeout(() => {
+        $('#loader-wrapper').css({'display':'none'})     
+    }, 1500)
+    
     var istoggle = false
-
+    
     $('.toggle-btn').click( () => {
         $('#sidebar-wrapper').css({'display':'inline-block'})
         if(istoggle) {
+            $('.overlay').removeClass("active")
             $('#sidebar-wrapper').removeClass("animated")
             $('#sidebar-wrapper').removeClass("slideInLeft")  
             $('#sidebar-wrapper').addClass("animated")
@@ -12,6 +17,7 @@ $(document).ready(function(){
             istoggle = false
         }
         else {
+            $('.overlay').addClass("active")
             $('#sidebar-wrapper').removeClass("animated")
             $('#sidebar-wrapper').removeClass("slideOutLeft")
             $('#sidebar-wrapper').addClass("animated")
@@ -20,12 +26,25 @@ $(document).ready(function(){
         }
     })
 
+    $('.overlay').click( () => {
+        $('.overlay').removeClass("active")
+        $('#sidebar-wrapper').removeClass("animated")
+        $('#sidebar-wrapper').removeClass("slideInLeft")  
+        $('#sidebar-wrapper').addClass("animated")
+        $('#sidebar-wrapper').addClass("slideOutLeft")  
+        istoggle = false
+    })
+
     $(window).resize( () => {
         var width_size = window.outerWidth
-
-        if(width_size) {
-            
+        
+        if(width_size > 1025) {
+            $('.overlay').removeClass("active")
+            $('#sidebar-wrapper').removeClass("animated")
+            $('#sidebar-wrapper').removeClass("slideInLeft")
+            istoggle = false
         }
+
     })
 
  })
